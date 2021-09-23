286537c69cc9d6c3e60b75ebbb6c1cea4879bc14
Gabriel Nicolitoiu <nicolitoiu.gabriel23@gmail.com>
2021-09-23 20:08:52 +0100

    Now the collection can be displayed more easily

--- src/components/collection.js
+++ src/components/collection.js
@@ -1,10 +1,27 @@
-import React from "react"
+import React, {useState,useEffect} from "react"
+import Card from './card'
+import {FaArrowRight} from 'react-icons/fa'
+import {FaArrowLeft} from 'react-icons/fa'
 
-export default function Card(){
+export default function Collection(props){
+    const [currentPage, setPage] = useState({first: 0, last:10})
+    const [cards, setCards]=useState(props.cards.slice(0,10));
+    const [loading,setLoading] = useState(true);
+
+    
     return(
-        <div className="collection">
-            
-            
+        <div className="cont-cont">
+            <FaArrowLeft className="right"  size="50"></FaArrowLeft>
+
+        <div className="collection-container">
+            {cards.length? cards.map(item=>{
+            return <Card classs={item.rarity} img ={item.image} dmg={item.damage} hp={item.hitpoints} lck={item.luck} />
+          }):console.log("errou")
+            }
+        </div>
+
+        <FaArrowRight className="right"  size="50"></FaArrowRight>
+       
         </div>
     )
 }
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,95 +1,149 @@
-import React, {useState,useEffect} from "react";
-import '../style/dashboard.css'
-import {useAuth} from '../AuthProvider'
-import {initFire} from '../Firebase.js'
-import { doc,getDoc, getFirestore,collection, setDoc } from "firebase/firestore";
-import { RiCopperCoinLine } from 'react-icons/ri';
-import {CgCardSpades} from 'react-icons/cg'
-import Card from '../components/card.js'
+import React, { useState, useEffect } from "react";
+import "../style/dashboard.css";
+import { useAuth } from "../AuthProvider";
+import { initFire } from "../Firebase.js";
+import {
+  doc,
+  getDoc,
+  getFirestore,
+  collection,
+  setDoc,
+} from "firebase/firestore";
+import { RiCopperCoinLine } from "react-icons/ri";
+import { CgCardSpades } from "react-icons/cg";
+
 import createCard from "../scripts/cardGenerator";
-
-
+import Card from "../components/card";
+import { FaArrowRight } from "react-icons/fa";
+import { FaArrowLeft } from "react-icons/fa";
 
 function Dashboard() {
-const [userData, setUserData] = useState({gold:0,collection:[]})
-const [parseCol, setCol] = useState({collection:[]})
-const{logOut, currentUser} = useAuth()
-const db = getFirestore(initFire)
-const docRef = doc(db, "users", currentUser.uid);
+  const [userData, setUserData] = useState({ collection: [] });
+  const [currentPage, setPage] = useState({ first: 0, last: 10 });
+  const { logOut, currentUser } = useAuth();
+  const db = getFirestore(initFire);
+  const docRef = doc(db, "users", currentUser.uid);
 
+  useEffect(() => {
+    getDocument();
+  }, []);
 
-useEffect(() => {
-  getDocument()
-},[]);
-
-
-
-async function saveChanges(){
-  const userRef = collection(db, "users");
-  await setDoc(doc(userRef, currentUser.uid), {
-    gold: userData.gold,
-    collection: JSON.stringify(userData.collection) });
-}
-
-
-async function  getDocument(){
-const docSnap = await getDoc(docRef);
-if (docSnap.exists()) {
-  if(docSnap.data().collection.length !== 0){
-    setUserData(prevState => {
-    return {...prevState, collection: JSON.parse(docSnap.data().collection) }
-    
-})
+  const forwardPage = () => {
+    let copyPage = { ...currentPage };
+    if (copyPage.last < 91) {
+      copyPage.first += 10;
+      copyPage.last += 10;
+      setPage(copyPage);
     }
- 
-   
-   
-  
-  setUserData(prevState => {
-    return {...prevState, gold: docSnap.data().gold }
-    
-})
-} else {
-  console.log("No such document!");
-}
-}
+  };
+  const backPage = () => {
+    let copyPage = { ...currentPage };
+    if (copyPage.first > 9) {
+      copyPage.first -= 10;
+      copyPage.last -= 10;
+      setPage(copyPage);
+    }
+  };
 
-const logState =()=>{
-  let copyData = {...userData};
-  let card = createCard();
-  copyData.gold = copyData.gold - 5;
-  copyData.collection.push(card);
-  setUserData(copyData)
-  saveChanges()
-  console.log(userData)
-}
-
-
-
-
-
-
-    return (
-      <div className="dashboard">
-          
-          <div className="menu">
-            <div className="stat"><RiCopperCoinLine className="stats-icon" size="30"/>{userData? userData.gold:"-"}</div>
-            <div className="stat"><CgCardSpades className="stats-icon" size="30"/>{userData? userData.collection.length +"/99":"-"}</div>
-              <button className="menuBtn" onClick={logOut}>Open Packs</button>
-              <button className="menuBtn" onClick={logOut}>Sign out</button>
-              <button className="menuBtn" onClick={logState}>New Card</button>
-              
-          
-            </div>
-          {userData? userData.collection.map(item=>{
-            return <Card classs={item.rarity} img ={item.image} dmg={item.damage} hp={item.hitpoints} lck={item.luck} />
-          }):console.log("errou")
-            }
-          
-         
-      
-      </div>
-    );
+  async function saveChanges() {
+    const userRef = collection(db, "users");
+    await setDoc(doc(userRef, currentUser.uid), {
+      gold: userData.gold,
+      collection: JSON.stringify(userData.collection),
+    });
   }
 
-  export default Dashboard
+  async function getDocument() {
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      if (docSnap.data().collection.length !== 0) {
+        setUserData((prevState) => {
+          return {
+            ...prevState,
+            collection: JSON.parse(docSnap.data().collection),
+          };
+        });
+      }
+
+      setUserData((prevState) => {
+        return { ...prevState, gold: docSnap.data().gold };
+      });
+    } else {
+      console.log("No such document!");
+    }
+  }
+
+  const logState = () => {
+    let copyData = { ...userData };
+    let card = createCard();
+    copyData.gold = copyData.gold - 5;
+    copyData.collection.push(card);
+    setUserData(copyData);
+    saveChanges();
+  };
+
+  function log() {
+    console.log(userData.collection.slice(0, 8));
+  }
+
+  return (
+    <div className="dashboard">
+      <div className="menu">
+        <div className="stat">
+          <RiCopperCoinLine className="stats-icon" size="30" />
+          {userData.gold ? userData.gold : "-"}
+        </div>
+        <div className="stat">
+          <CgCardSpades className="stats-icon" size="30" />
+          {userData.collection ? userData.collection.length + "/100" : "-"}
+        </div>
+        <button className="menuBtn" onClick={logOut}>
+          Open Packs
+        </button>
+        <button className="menuBtn" onClick={logOut}>
+          Sign out
+        </button>
+        <button className="menuBtn" onClick={logState}>
+          New Card
+        </button>
+        <button className="menuBtn" onClick={log}>
+          teasfd
+        </button>
+      </div>
+
+      <div className="cont-cont">
+        <FaArrowLeft
+          onClick={backPage}
+          className="right"
+          size="50"
+        ></FaArrowLeft>
+
+        <div className="collection-container">
+          {userData.collection
+            ? userData.collection
+                .slice(currentPage.first, currentPage.last)
+                .map((item) => {
+                  return (
+                    <Card
+                      classs={item.rarity}
+                      img={item.image}
+                      dmg={item.damage}
+                      hp={item.hitpoints}
+                      lck={item.luck}
+                    />
+                  );
+                })
+            : console.log("errou")}
+        </div>
+
+        <FaArrowRight
+          onClick={forwardPage}
+          className="right"
+          size="50"
+        ></FaArrowRight>
+      </div>
+    </div>
+  );
+}
+
+export default Dashboard;
