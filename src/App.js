9371870c0aae6f7a30ac407bda8fbb15a59cc877
Gabriel Nicolitoiu <nicolitoiu.gabriel23@gmail.com>
2021-11-18 01:16:47 +0000

    hash router added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route} from "react-router-dom"
+import { HashRouter as Router, Switch, Route} from "react-router-dom"
 import Login from "./pages/login"
 import {AuthProvider} from './AuthProvider.js';
 import { ColProvider } from "./CollectionProvider";
